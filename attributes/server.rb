639e34c3cf151329766ad19cec28d626b8845bcb
John E. Vincent <lusis.org+github.com@gmail.com>
2012-06-13 08:38:45 -0400

    making source default for server

--- attributes/server.rb
+++ attributes/server.rb
@@ -1,7 +1,7 @@
 default[:logstash][:server][:version] = "1.1.0.1"
 default[:logstash][:server][:source_url] = "http://semicomplete.com/files/logstash/logstash-#{node[:logstash][:server][:version]}-monolithic.jar"
 default[:logstash][:server][:checksum] = "9808bd88725f3166c26d21b96226da3e36dad089cea91f6e22a645365724e4d9"
-default[:logstash][:server][:install_method] = "jar" # Either `source` or `jar`
+default[:logstash][:server][:install_method] = "source" # Either `source` or `jar`
 default[:logstash][:server][:base_config] = "server.conf.erb"
 default[:logstash][:server][:xms] = "1024M"
 default[:logstash][:server][:xmx] = "1024M"
