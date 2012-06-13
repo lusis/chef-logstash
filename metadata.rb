b3c67e56cc10710e15a7016f8b14f7fec2e76ad7
John E. Vincent <lusis.org+github.com@gmail.com>
2012-06-13 06:43:39 -0400

    bumping version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "lusis.org+github.com@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures logstash"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.2.0"
 %w{apache2 php build-essential git runit python java ant}.each do |cb|
   depends cb
 end
