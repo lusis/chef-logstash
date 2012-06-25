5b896cb63ae2cb7daac064508e9805ffed872f81
Bryan Berry <bryan.berry@gmail.com>
2012-06-25 15:33:31 +0200

    add in basic rhel support change some symbols as part of process to comply w/ foodcritic rules

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,8 +1,11 @@
-default[:logstash][:basedir] = "/opt/logstash"
-default[:logstash][:user] = "logstash"
-default[:logstash][:group] = "logstash"
+default['logstash']['basedir'] = "/opt/logstash"
+default['logstash']['user'] = "logstash"
+default['logstash']['group'] = "logstash"
+default['logstash']['log_dir'] = "/var/log/logstash"
+default['logstash']['pid_dir'] = "/var/run/logstash"
+
 
 # roles/flags for various search/discovery
-default[:logstash][:graphite_role] = "graphite_server"
-default[:logstash][:elasticsearch_role] = "elasticsearch_server"
-default[:logstash][:elasticsearch_cluster] = "logstash"
+default['logstash']['graphite_role'] = "graphite_server"
+default['logstash']['elasticsearch_role'] = "elasticsearch_server"
+default['logstash']['elasticsearch_cluster'] = "logstash"
--- recipes/server.rb
+++ recipes/server.rb
@@ -1,78 +1,127 @@
 #
+# Author:: John E. Vincent
+# Author:: Bryan W. Berry (<bryan.berry@gmail.com>)
+# Copyright 2012, John E. Vincent
+# Copyright 2012, Bryan W. Berry
+# License: Apache 2.0
 # Cookbook Name:: logstash
 # Recipe:: server
 #
 #
+
 include_recipe "logstash::default"
+include_recipe "logrotate"
+
 
 graphite_server = search(:node, "role:#{node[:logstash][:graphite_role]} AND chef_environment:#{node.chef_environment}")
 elasticsearch_server = search(:node, "role:#{node[:logstash][:elasticsearch_role]} AND chef_environment:#{node.chef_environment}")
 
-directory "#{node[:logstash][:basedir]}/server" do
+#create directory for logstash
+directory "#{node['logstash']['home']}/server" do
   action :create
   mode "0755"
-  owner "#{node[:logstash][:user]}"
-  group "#{node[:logstash][:group]}"
+  owner "#{node['logstash']['user']}"
+  group "#{node['logstash']['group']}"
 end
 
-%w{bin etc lib log tmp}.each do |ldir|
-  directory "#{node[:logstash][:basedir]}/server/#{ldir}" do
+%w{bin etc lib log tmp patterns haproxylog_db }.each do |ldir|
+  
+  directory "#{node['logstash']['basedir']}/server/#{ldir}" do
     action :create
     mode "0755"
-    owner "#{node[:logstash][:user]}"
-    group "#{node[:logstash][:group]}"
+    owner "#{node['logstash']['user']}"
+    group "#{node['logstash']['group']}"
   end
 
   link "/var/lib/logstash/#{ldir}" do
-    to "#{node[:logstash][:basedir]}/server/#{ldir}"
+    to "#{node['logstash']['basedir']}/server/#{ldir}"
   end
 end
 
-directory "#{node[:logstash][:basedir]}/server/etc/conf.d" do
-  action :create
-  mode "0755"
-  owner "#{node[:logstash][:user]}"
-  group "#{node[:logstash][:group]}"
-end
 
-directory "#{node[:logstash][:basedir]}/server/etc/patterns" do
-  action :create
-  mode "0755"
-  owner "#{node[:logstash][:user]}"
-  group "#{node[:logstash][:group]}"
-end
-
-if node[:logstash][:server][:install_method] == "jar"
-  remote_file "#{node[:logstash][:basedir]}/server/lib/logstash-#{node[:logstash][:server][:version]}.jar" do
+#installation
+if node['logstash']['server']['install_method'] == "jar"
+  remote_file "#{node['logstash']['basedir']}/server/lib/logstash-#{node['logstash']['server']['version']}.jar" do
     owner "root"
     group "root"
     mode "0755"
-    source "#{node[:logstash][:server][:source_url]}"
-    checksum "#{node[:logstash][:server][:checksum]}"
+    source "#{node['logstash']['server']['source_url']}"
+    #checksum "#{node['logstash']['server']['checksum']}"
+  not_if {File.exists?("#{node['logstash']['basedir']}/server/lib/logstash-#{node['logstash']['server']['version']}.jar")}
   end
-  link "#{node[:logstash][:basedir]}/server/lib/logstash.jar" do
-    to "#{node[:logstash][:basedir]}/server/lib/logstash-#{node[:logstash][:server][:version]}.jar"
-    notifies :restart, "service[logstash_server]"
+  link "#{node['logstash']['basedir']}/server/lib/logstash.jar" do
+    to "#{node['logstash']['basedir']}/server/lib/logstash-#{node['logstash']['server']['version']}.jar"
+    #notifies :restart, "service[logstash_server]"
   end
 else
   include_recipe "logstash::source"
 
-  link "#{node[:logstash][:basedir]}/server/lib/logstash.jar" do
-    to "#{node[:logstash][:basedir]}/source/build/logstash-#{node[:logstash][:source][:sha]}-monolithic.jar"
-    notifies :restart, "service[logstash_server]"
+  link "#{node['logstash']['basedir']}/server/lib/logstash.jar" do
+    to "#{node['logstash']['basedir']}/source/build/logstash-#{node['logstash']['source']['sha']}-monolithic.jar"
+    #notifies :restart, "service[logstash_server]"
   end
 end
 
-template "#{node[:logstash][:basedir]}/server/etc/logstash.conf" do
-  source "#{node[:logstash][:server][:base_config]}"
-  owner "#{node[:logstash][:user]}"
-  group "#{node[:logstash][:group]}"
-  mode "0644"
-  variables(:graphite_server => graphite_server,
-            :enable_embedded_es => node[:logstash][:server][:enable_embedded_es],
-            :es_server => elasticsearch_server,
-            :es_cluster => node[:logstash][:elasticsearch_cluster])
-  notifies :restart, "service[logstash_server]"
+directory "#{node['logstash']['basedir']}/server/etc/conf.d" do
+  action :create
+  mode "0755"
+  owner "#{node['logstash']['user']}"
+  group "#{node['logstash']['group']}"
 end
 
-runit_service "logstash_server"
+template "#{node['logstash']['basedir']}/server/etc/logstash.conf" do
+  source "#{node['logstash']['server']['base_config']}"
+  owner "#{node['logstash']['user']}"
+  group "#{node['logstash']['group']}"
+  mode "0644"
+  variables(:graphite_server => graphite_server,
+           :enable_embedded_es => node['logstash']['server']['enable_embedded_es'],
+           :es_server => elasticsearch_server,
+           :es_cluster => node['logstash']['elasticsearch_cluster'])
+  notifies :restart, "service[logstash_server]"
+  action :create_if_missing
+end
+
+
+case platform_family?
+when "debian"
+  runit_service "logstash_server"
+when "rhel"
+  template "/etc/init.d/logstash" do
+    source "init.erb"
+    owner "root"
+    group "root"
+    mode "0774"
+  end
+  service "logstash" do
+    supports :restart => true, :reload => true, :status => true
+    action :enable
+  end
+end
+
+#create pattern_file  for haproxy
+template "/opt/logstash/server/etc/patterns/haproxy.conf" do
+    source "haproxy_pattern.el.erb"
+    owner "root"
+    group "root"
+    mode "0774"
+end
+
+#set logrotate  for /opt/logstash/server/haproxylog_db
+logrotate_app "haproxylog_db" do
+  path "#{node['logstash']['server']['logrotate_target']}"
+  frequency "daily"
+  rotate "30"
+  notifies :restart, "service[rsyslog]"
+end
+
+
+logrotate_app "logstash" do
+  path "/var/log/logstash/*.log"
+  frequency "daily"
+  rotate "30"
+  notifies :restart, "service[rsyslog]"
+end
+
+
+
