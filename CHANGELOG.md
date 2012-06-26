532540acca0a6742562932088f6761d3c74b86d0
Bryan Berry <bryan.berry@gmail.com>
2012-06-26 17:38:29 +0200

    add in travis-ci support

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,7 @@ New features
 	and outputs
 	* redhat-family support
 	* change default version of logstash to 1.1.1preview
+	* add in Travis-CI support
 
 Bug fixes
 	* keep apache default site from obscuring kibana
--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,7 @@ license          "Apache 2.0"
 description      "Installs/Configures logstash"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.2.1"
+
 %w{apache2 php build-essential git runit python java ant logrotate}.each do |cb|
   depends cb
 end
