c1086c5c588801bc905d79ee1d061cce5a0f62f1
Bryan Berry <bryan.berry@gmail.com>
2012-06-26 17:14:07 +0200

    change to using the jar for agents because jpackage is not working on rhel 6.2

--- attributes/agent.rb
+++ attributes/agent.rb
@@ -1,7 +1,7 @@
 default['logstash']['agent']['version'] = "1.1.1"
 default['logstash']['agent']['source_url'] = 'http://databits.net/petef/tmp/logstash-1.1.1-pre-monolithic-jruby1.7.0pre1.jar'
 default['logstash']['agent']['checksum'] = '6ca41718706c118ee6abb339bec9225b5d56cc3dc258d5053e64d00e24cdb918'
-default['logstash']['agent']['install_method'] = "source" # Either `source` or `jar`
+default['logstash']['agent']['install_method'] = "jar" # Either `source` or `jar`
 default['logstash']['agent']['base_config'] = "agent.conf.erb"
 default['logstash']['agent']['xms'] = "384M"
 default['logstash']['agent']['xmx'] = "384M"
