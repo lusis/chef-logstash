012b3cbfa36bdacfabee5b74e9e813338de843db
Bryan Berry <bryan.berry@gmail.com>
2012-06-29 16:19:53 +0200

    add rabbitmq as a dependency

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,6 @@ description      "Installs/Configures logstash"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.2.1"
 
-%w{apache2 php build-essential git runit python java ant logrotate}.each do |cb|
+%w{apache2 php build-essential git runit python java ant logrotate rabbitmq}.each do |cb|
   depends cb
 end
