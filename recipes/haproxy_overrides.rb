6b54b38544522d20f3da0f80040ce2715625f60a
Bryan Berry <bryan.berry@gmail.com>
2012-07-13 16:32:01 +0200

    use attribute for base_dir rather than hard-coding it

--- recipes/haproxy_overrides.rb
+++ recipes/haproxy_overrides.rb
@@ -1,6 +1,6 @@
 # Create pattern_file  for haproxy with overrides for capturing request
 # and response headers
-cookbook_file "/opt/logstash/server/etc/patterns/haproxy" do
+cookbook_file "#{node['logstash']['basedir']}/server/etc/patterns/haproxy" do
     source "haproxy"
     owner node['logstash']['user']
     group node['logstash']['group']
