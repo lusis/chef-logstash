a0f47f16e9d27a0c022bec7b27f8fab382d9a40d
Bryan Berry <bryan.berry@gmail.com>
2012-08-13 08:56:51 +0200

    remove logrotate dependency

--- metadata.rb
+++ metadata.rb
@@ -3,8 +3,8 @@ maintainer_email "lusis.org+github.com@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures logstash"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.3"
+version          "0.2.4"
 
-%w{apache2 php build-essential git runit python java ant logrotate rabbitmq}.each do |cb|
+%w{apache2 php build-essential git runit python java ant rabbitmq}.each do |cb|
   depends cb
 end
--- recipes/server.rb
+++ recipes/server.rb
@@ -10,7 +10,6 @@
 #
 
 include_recipe "logstash::default"
-include_recipe "logrotate"
 include_recipe "rabbitmq"
 
 if Chef::Config[:solo] 
@@ -118,11 +117,10 @@ template "#{node['logstash']['basedir']}/server/etc/logstash.conf" do
   action :create
 end
 
-logrotate_app "logstash" do
-  path "/var/log/logstash/*.log"
-  frequency "daily"
-  rotate "30"
-  create    "664 #{node['logstash']['user']} #{node['logstash']['user']}"
+cron "compress and remove logs rotated by log4j" do
+  minute "0"
+  hour   "0"
+  command  "find /var/log/logstash -name '*.gz' -mtime +30 -exec rm -f '{}' \\; ; \
+  find /var/log/logstash ! -name '*.gz' -mtime +2 -exec gzip '{}' \\;"
 end
-
 
