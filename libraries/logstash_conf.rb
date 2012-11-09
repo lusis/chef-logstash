2a3b30eb5f9df1eb247032782a852d70d3123d8b
Xabier de Zuazo <xabier@zuazo.org>
2012-11-09 16:53:20 +0100

    Added support for grok pattern files generation from node attributes

--- libraries/logstash_conf.rb
+++ libraries/logstash_conf.rb
@@ -1,4 +1,6 @@
 
+require 'rubygems'
+
 class Erubis::RubyEvaluator::LogstashConf
 
   private
@@ -31,12 +33,19 @@ class Erubis::RubyEvaluator::LogstashConf
 
   public
   
-  def self.section_to_str(section)
+  def self.section_to_str(section, version=nil, patterns_dir=nil)
     result = []
+    patterns_dir_plugins = [ 'grok' ]
+    unless version.nil?
+      patterns_dir_plugins << 'multiline' if Gem::Version.new(version) >= Gem::Version.new('1.1.2')
+    end
     section.each do |output|
       output.sort.each do |name, hash|
         result << ''
         result << '  ' + name.to_s + ' {'
+        if patterns_dir_plugins.include?(name.to_s) and not patterns_dir.nil? and not hash.has_key?('patterns_dir')
+          result << '    ' + key_value_to_str('patterns_dir', patterns_dir)
+        end
         hash.sort.each do |k,v|
           result << '    ' + key_value_to_str(k, v)
         end
