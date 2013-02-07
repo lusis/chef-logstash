33485d93ddf762ee7bfdb3f1ea5ce49ce94ce0d3
Bryan Berry <bryan.berry@gmail.com>
2013-02-07 18:24:44 +0100

    remove unnecessary attrs and add some needed ones

--- attributes/kibana.rb
+++ attributes/kibana.rb
@@ -1,6 +1,8 @@
 default['logstash']['kibana']['repo'] = 'git://github.com/rashidkpc/Kibana.git'
 default['logstash']['kibana']['sha'] = '376676cf33e5a2ce932604e1159a00db0ad17dda'
 default['logstash']['kibana']['apache_template'] = 'kibana.conf.erb'
+default['logstash']['kibana']['basedir'] = "#{node['logstash']['basedir']}/kibana"
+default['logstash']['kibana']['home'] = "#{node['logstash']['kibana']['basedir']}/current"
 default['logstash']['kibana']['config'] = 'kibana-config.php.erb'
 default['logstash']['kibana']['server_name'] = node['ipaddress']
 default['logstash']['kibana']['http_port'] = 80
@@ -11,12 +13,5 @@ default['apache']['default_site_enabled'] = false
 
 #Smart_index_pattern = 'logstash-%Y.%m.%d'
 default['logstash']['kibana']['smart_index_pattern'] = 'logstash-%Y.%m.%d' 
-
-
 default['logstash']['kibana']['language'] = "ruby" 
-
-default['logstash']['kibana']['ruby_version']['base_dir'] = "/opt/logstash"
-default['logstash']['kibana']['ruby_version']['version'] = "ruby"
-default['logstash']['kibana']['ruby_version']['port'] = 80
-default['logstash']['kibana']['ruby_version']['home'] = "#{default['logstash']['kibana']['ruby_version']['base_dir']}/kibana-#{default['logstash']['kibana']['ruby_version']['version']}"
 
