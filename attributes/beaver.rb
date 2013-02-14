6334350eb78e8943e7eebe993c3250666b0d27e2
Bryan Berry <bryan.berry@gmail.com>
2013-02-14 18:23:41 +0100

    install additional pip_packages needed by beaver

--- attributes/beaver.rb
+++ attributes/beaver.rb
@@ -1,10 +1,7 @@
 
-default['logstash']['beaver']['repo'] = "beaver==22"
-
+default['logstash']['beaver']['pip_packages'] = [ "beaver==22", "pyzmq==2.1.11" ]
 default['logstash']['beaver']['server_role'] = "logstash_server"
-
 default['logstash']['beaver']['server_ipaddress'] = nil
-
 default['logstash']['beaver']['inputs'] = []
 default['logstash']['beaver']['outputs'] = []
 
