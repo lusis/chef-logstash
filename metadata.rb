7d4d86af33389355f5e77172d3e9952d4e89dead
Bryan Berry <bryan.berry@gmail.com>
2013-02-18 07:41:24 +0100

    add yumrepo to list of depends

--- metadata.rb
+++ metadata.rb
@@ -6,15 +6,10 @@ description      "Installs/Configures logstash"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.5.4"
 
-supports         "ubuntu"
-supports         "debian"
+%w{ ubuntu debian redhat centos scientific amazon fedora }.each do |os|
+  supports os
+end
 
-supports         "redhat"
-supports         "centos"
-supports         "scientific"
-supports         "amazon"
-supports         "fedora"
-
-%w{ apache2 php build-essential git rvm runit python java ant logrotate rabbitmq }.each do |ckbk|
+%w{ apache2 php build-essential git rvm runit python java ant logrotate rabbitmq yumrepo }.each do |ckbk|
   depends ckbk
 end
