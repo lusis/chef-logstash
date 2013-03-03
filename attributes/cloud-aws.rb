7143194fabef66d39b15c0a67c90d1c25ae05855
James Brook <james_brook@yahoo.co.uk>
2013-03-03 11:14:35 +0100

    Removed unneeded attribute.

--- attributes/cloud-aws.rb
+++ attributes/cloud-aws.rb
@@ -1,7 +1,6 @@
 default['logstash']['es-cloud-aws-plugin']['version'] = '1.11.0'
 default['logstash']['es-cloud-aws-plugin']['download_url'] = "https://download.elasticsearch.org/elasticsearch/elasticsearch-cloud-aws/elasticsearch-cloud-aws-#{node['logstash']['es-cloud-aws-plugin']['version']}.zip"
 
-default['logstash']['elasticsearch']['cluster_name'] = 'logstash'
 default['logstash']['elasticsearch']['cloud']['aws']['access_key'] = ''
 default['logstash']['elasticsearch']['cloud']['aws']['secret_key'] = ''
 default['logstash']['elasticsearch']['cloud']['aws']['region'] = 'us-east'
