abbd41f57848dd9a815df3686dd4a48d324385e3
Tom Howe <turtlebender@gmail.com>
2013-04-02 13:01:45 -0500

    fixes for chef 11 warning
    when using platform family in attributes, you need to call:
    
    node['platform_family']
    
    otherwise you get handled by the missing_method handler. Also, this
    sets default instead of the value on the node for libzmq

---
+++ default.rb
@@ -0,0 +1,27 @@
+default['logstash']['basedir'] = '/opt/logstash'
+default['logstash']['user'] = 'logstash'
+default['logstash']['group'] = 'logstash'
+default['logstash']['join_groups'] = []
+default['logstash']['log_dir'] = '/var/log/logstash'
+default['logstash']['pid_dir'] = '/var/run/logstash'
+default['logstash']['create_account'] = true
+
+# roles/flags for various search/discovery
+default['logstash']['graphite_role'] = 'graphite_server'
+default['logstash']['graphite_query'] = "roles:#{node['logstash']['graphite_role']} AND chef_environment:#{node.chef_environment}"
+default['logstash']['elasticsearch_role'] = 'elasticsearch_server'
+default['logstash']['elasticsearch_query'] = "roles:#{node['logstash']['elasticsearch_role']} AND chef_environment:#{node.chef_environment}"
+default['logstash']['elasticsearch_cluster'] = 'logstash'
+default['logstash']['elasticsearch_ip'] = ''
+default['logstash']['elasticsearch_port'] = ''
+default['logstash']['graphite_ip'] = ''
+
+default['logstash']['patterns'] = {}
+default['logstash']['install_zeromq'] = false
+
+case node['platform_family']
+when "rhel"
+  default['logstash']['zeromq_packages'] = [ "zeromq",  "zeromq-devel"]
+when "debian"
+  default['logstash']['zeromq_packages'] = [ "zeromq",  "libzmq-dev"]
+end
