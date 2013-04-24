5b74f103444bdab3a09b2434e8a469c6b3210bb9
Chris Lundquist <chris.lundquist@bluebox.net>
2013-04-24 16:36:05 -0700

    [metadata] bump to 0.6.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "lusis.org+github.com@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures logstash"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.5.9"
+version          "0.6.0"
 
 %w{ ubuntu debian redhat centos scientific amazon fedora }.each do |os|
   supports os
