1d75a5553cdfec54080147231c38ed3114b03bb4
Will Salt <will@spaceapegames.com>
2013-05-16 11:54:21 +0100

    removed junk lines from metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -5,8 +5,6 @@ license          "Apache 2.0"
 description      "Installs/Configures logstash"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.6.1"
-=======
-
 
 %w{ ubuntu debian redhat centos scientific amazon fedora }.each do |os|
   supports os
