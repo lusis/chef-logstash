381a91bd51da8f867f01ebe466d40b7c75aa89ef
Nicholas Padilla <nicholas@mindsignited.com>
2013-10-23 22:53:26 -0600

    Added some more logrotate control options, since one size doesn't fit all on logging requirements.  :)

--- recipes/server.rb
+++ recipes/server.rb
@@ -185,8 +185,12 @@ end
 
 logrotate_app "logstash_server" do
   path "#{node['logstash']['log_dir']}/*.log"
-  frequency "daily"
-  rotate "30"
+  if node['logstash']['logging']['useFileSize']
+    size node['logstash']['logging']['maxSize']
+  else
+    frequency node['logstash']['logging']['rotateFrequency']
+  end
+  rotate node['logstash']['logging']['maxBackup']
   options node['logstash']['server']['logrotate']['options']
   create "664 #{node['logstash']['user']} #{node['logstash']['group']}"
 end
