b39867f0e8ce861701b729bf968d90413682c425
Paul Czarkowski <paul.czarkowski@rackspace.com>
2014-03-24 11:49:43 -0500

    adding chefspec tests

--- attributes/server.rb
+++ attributes/server.rb
@@ -5,11 +5,11 @@ default['logstash']['instance']['server'] = node['logstash']['instance']['defaul
 
 # override any defaults here.
 
-override['logstash']['instance']['server']['config_templates']   = {
+set['logstash']['instance']['server']['config_templates']   = {
   'input_syslog' => 'config/input_syslog.conf.erb',
   'output_stdout' => 'config/output_stdout.conf.erb',
   'output_elasticsearch' => 'config/output_elasticsearch.conf.erb'
 }
-override['logstash']['instance']['server']['pattern_templates']  = { 'default' => 'patterns/custom_patterns.erb' }
-override['logstash']['instance']['server']['elasticsearch_ip']  = '127.0.0.1'
-override['logstash']['instance']['server']['enable_embedded_es'] = true
+set['logstash']['instance']['server']['pattern_templates']  = { 'default' => 'patterns/custom_patterns.erb' }
+set['logstash']['instance']['server']['elasticsearch_ip']  = '127.0.0.1'
+set['logstash']['instance']['server']['enable_embedded_es'] = true
--- test/unit/spec/server_spec.rb
+++ test/unit/spec/server_spec.rb
@@ -2,12 +2,30 @@
 require_relative 'spec_helper'
 
 describe 'logstash::server' do
-  before { logstash_stubs }
   describe 'ubuntu' do
-    let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
+    let(:runner) { ChefSpec::Runner.new(::UBUNTU_OPTS) }
+    let(:node) { runner.node }
+    let(:chef_run) do
+      # runner.node.set['logstash'] ...
+      runner.converge(described_recipe)
+    end
+    include_context 'stubs-common'
 
-    it 'writes some chefspec code' do
-      pending 'todo'
+    it 'calls the logstash_instance LWPR' do
+      expect(chef_run).to create_logstash_instance('server')
+    end
+
+    it 'calls the logstash_config LWPR' do
+      expect(chef_run).to create_logstash_config('server')
+    end
+
+    it 'calls the logstash_pattern LWPR' do
+      expect(chef_run).to create_logstash_pattern('server')
+    end
+
+    it 'calls the logstash_instance LWPR' do
+      expect(chef_run).to enable_logstash_service('server')
+      expect(chef_run).to start_logstash_service('server')
     end
 
   end
