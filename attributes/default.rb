51f5b668622c1c8027c632ccb274571d123cd863
Sal Poliandro <popsikle@gmail.com>
2014-05-29 10:37:56 -0400

    made memory check based on values used to catch the 1.5gb machine errors

--- attributes/default.rb
+++ attributes/default.rb
@@ -34,7 +34,7 @@ default['logstash']['instance']['default']['plugins_install_type']   = 'tarball'
 default['logstash']['instance']['default']['plugins_check_if_installed']  = 'lib/logstash/filters/translate.rb'
 
 default['logstash']['instance']['default']['log_file']       = 'logstash.log'
-if node['memory']['total'].to_i > 1020540
+if ((node['memory']['total'].to_i * 0.6).floor / 1024 ) > 1024
   default['logstash']['instance']['default']['xms']        = '1024M'
 else
   default['logstash']['instance']['default']['xms']        = "#{(node['memory']['total'].to_i * 0.5).floor / 1024}M"
