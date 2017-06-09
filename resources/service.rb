61ff175b1014b30a12f3d709efc0c8c70e08bb7e
Vitaly Gorodetsky <gorodetsky@gmail.com>
2017-06-09 17:01:22 +0300

    renames internal attribute name from `method` to `init_method`
    * Not a breaking change
    * Right now the cookbook fails to compile with Chef13

--- resources/service.rb
+++ resources/service.rb
@@ -11,7 +11,7 @@ default_action :enable if defined?(default_action)
 
 attribute :instance, kind_of: String, name_attribute: true
 attribute :service_name, kind_of: String
-attribute :method, kind_of: String
+attribute :init_method, kind_of: String
 attribute :command, kind_of: String
 attribute :args, kind_of: Array
 attribute :description, kind_of: String
