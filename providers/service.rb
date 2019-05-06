f7dcb6dcc8b44cd0e2b88083b1e60c2dcf693d0c
Antoine Reversat <a.reversat@gmail.com>
2019-05-06 12:35:34 -0400

    Make sure we don't execute the default runit service action (enable) when creating the runit service to run another action

--- providers/service.rb
+++ providers/service.rb
@@ -226,7 +226,9 @@ def service_action(action)
     end
   when 'runit'
     @run_context.include_recipe 'runit::default'
-    sv = runit_service svc[:service_name]
+    sv = runit_service svc[:service_name] do
+        action(:nothing)
+    end
   end
   sv.run_action(action)
   sv.updated_by_last_action?
